e9b72a4197791d6801223badf7cb0133324afbaf
Usuario <Usuario@DAVID>
2023-06-20 18:59:06 +0200

    Commit Final

---
+++ Web Personal/mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "David Cabrero - Contacto"; //letter subject
+	$to = 'davidcabrerojimenez@gmail.com'; // Enter Your E-mail
+	$from = 'davidcabrerojimenez@gmail.com'; // Admin e-mail
+	$fromName = 'David Cabrero'; // Your company name
+	$charset = 'UTF-8';
+?>
